54aed0b9ef8963510d6b1e8e0da81fd8427f5eae
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2021-06-06 20:43:07 +0900

    学生に配布するための準備。

--- 右クリックで管理者として実行してね.bat
+++ 右クリックで管理者として実行してね.bat
@@ -8,16 +8,6 @@ pushd %CURRENT_DIR%
 @call .\Sample\cleanup.bat
 @call .\Sample\setup.bat
 
-@rem maxScriptをコピー
-@copy "%CURRENT_DIR%GameTemplate\tools\3dsMaxScripts\tkExporter.ms" "c:\Program Files\Autodesk\3ds Max 2015\scripts\"
-@copy "%CURRENT_DIR%GameTemplate\tools\3dsMaxScripts\tkExporter.ms" "c:\Program Files\Autodesk\3ds Max 2016\scripts\"
-@copy "%CURRENT_DIR%GameTemplate\tools\3dsMaxScripts\tkExporter.ms" "c:\Program Files\Autodesk\3ds Max 2017\scripts\"
-@copy "%CURRENT_DIR%GameTemplate\tools\3dsMaxScripts\tkExporter.ms" "c:\Program Files\Autodesk\3ds Max 2018\scripts\"
-@copy "%CURRENT_DIR%GameTemplate\tools\3dsMaxScripts\tkExporter.ms" "c:\Program Files\Autodesk\3ds Max 2021\scripts\"
-@copy "%CURRENT_DIR%GameTemplate\tools\3dsMaxScripts\tkExporter.ms" "c:\Program Files\Autodesk\3ds Max 2022\scripts\"
-
-
-
 @popd
 @echo セットアップ終了
 
