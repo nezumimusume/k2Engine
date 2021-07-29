ef113920ff49d0e8d03cc56e5164dd9b9f2013c0
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2021-07-29 15:37:54 +0900

    StarRenderの上げ忘れ

---
+++ Sample/DemoGame_00/Game/StarRender.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "StarRender.h"
+
+bool StarRender::Start()
+{
+	m_modelRender.Init(
+		"Assets/modelData/star.tkm",
+		nullptr,
+		0,
+		enModelUpAxisZ,
+		false,
+		m_maxStar
+	);
+	return true;
+}
+void StarRender::Update()
+{
+}
+void StarRender::Render(RenderContext& rc)
+{
+	m_modelRender.Draw(rc);
+}
