59dd4a8d0151f85af3b7c66a8dfa4621e66c5dfa
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2021-08-11 07:54:37 +0900

    上げ忘れていたファイルをコミット

---
+++ GameTemplate/k2Engine/graphics/postEffect/PostEffectComponentBase.cpp
@@ -0,0 +1,44 @@
+#include "k2EnginePreCompile.h"
+#include "PostEffectComponentBase.h"
+
+void PostEffectComponentBase::Init(
+	RenderTarget& mainRenderTarget,
+	RenderTarget& zprepassRenderTarget,
+	RenderTarget& normalRenderTarget,
+	RenderTarget& metallicSmoothRenderTarget,
+	RenderTarget& albedoRenderTarget
+)
+{
+	OnInit(
+		mainRenderTarget, 
+		zprepassRenderTarget, 
+		normalRenderTarget, 
+		metallicSmoothRenderTarget, 
+		albedoRenderTarget
+	);
+
+	if (IsCopyResultTextureToMainRenderTarget()) {
+		SpriteInitData initData;
+		initData.m_width = mainRenderTarget.GetWidth();
+		initData.m_height = mainRenderTarget.GetHeight();
+		initData.m_colorBufferFormat[0] = mainRenderTarget.GetColorBufferFormat();
+		initData.m_fxFilePath = "Assets/shader/Sprite.fx";
+		initData.m_textures[0] = &GetResultTexture();
+		m_copyMainRtSprite.Init(initData);
+	}
+}
+
+void PostEffectComponentBase::Render(RenderContext& rc, RenderTarget& mainRenderTarget)
+{
+	OnRender(rc, mainRenderTarget);
+	if (IsCopyResultTextureToMainRenderTarget()) {
+		// レンダリングターゲットとして利用できるまで待つ
+		rc.WaitUntilToPossibleSetRenderTarget(mainRenderTarget);
+		// レンダリングターゲットを設定
+		rc.SetRenderTargetAndViewport(mainRenderTarget);
+		// ポストエフェクトの結果をメインレンダリングターゲットに反映。
+		m_copyMainRtSprite.Draw(rc);
+		// レンダリングターゲットへの書き込み終了待ち
+		rc.WaitUntilFinishDrawingToRenderTarget(mainRenderTarget);
+	}
+}
---
+++ GameTemplate/k2Engine/graphics/postEffect/PostEffectComponentBase.h
@@ -0,0 +1,32 @@
+#pragma once
+
+class PostEffectComponentBase : public Noncopyable {
+public:
+	void Init(
+		RenderTarget& mainRenderTarget,
+		RenderTarget& zprepassRenderTarget,
+		RenderTarget& normalRenderTarget,
+		RenderTarget& metallicSmoothRenderTarget,
+		RenderTarget& albedoRenderTarget
+	);
+	void Render(RenderContext& rc, RenderTarget& mainRenderTarget);
+private:
+	virtual void OnInit(
+		RenderTarget& mainRenderTarget,
+		RenderTarget& zprepassRenderTarget,
+		RenderTarget& normalRenderTarget,
+		RenderTarget& metallicSmoothRenderTarget,
+		RenderTarget& albedoRenderTarget) = 0;
+	virtual void OnRender(RenderContext& rc, RenderTarget& mainRenderTarget) = 0;
+	virtual Texture& GetResultTexture() = 0;
+	/// <summary>
+	/// ポストの結果をメインレンダリングターゲットにコピーする？
+	/// </summary>
+	/// <returns></returns>
+	virtual bool IsCopyResultTextureToMainRenderTarget() const
+	{
+		return false;
+	}
+private:
+	Sprite m_copyMainRtSprite;	// メインレンダリングターゲットに描画するためのスプライト。
+};
