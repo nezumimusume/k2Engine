4492778f71c54171853de1d5c86995c1311b04b2
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2021-10-05 16:04:16 +0900

    コードの上げ忘れ

---
+++ GameTemplate/k2EngineLow/geometry/AABB.h
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace nsK2EngineLow {
+	/// <summary>
+	/// AABB
+	/// </summary>
+	class AABB {
+	private:
+		Vector3 m_max;					// AABBを構成するボックスの最大座標。
+		Vector3 m_min;					// AABBを構成するボックスの最小座標。
+		Vector3 m_vertexPosition[8];	// AABBを構成する8頂点の座標。
+	public:
+		/// <summary>
+		/// AABBの情報を初期化する。
+		/// </summary>
+		/// <param name="vMax">最大値</param>
+		/// <param name="vMin">最小値</param>
+		void Init(const Vector3& vMax, const Vector3& vMin);
+		/// <summary>
+		/// AABBを構成する8頂点のワールド空間での座標を計算する。
+		/// </summary>
+		/// <param name="pos"></param>
+		/// <param name="mWorld"></param>
+		void CalcVertexPositions(Vector3* pos, const Matrix& mWorld);
+		/// <summary>
+		/// AABBの最大値を取得。
+		/// </summary>
+		const Vector3& GetMax() const
+		{
+			return m_max;
+		}
+		/// <summary>
+		/// AABBの最小値を取得。
+		/// </summary>
+		/// <returns>最小値</returns>
+		const Vector3& GetMin() const
+		{
+			return m_min;
+		}
+	};
+}
---
+++ GameTemplate/k2EngineLow/geometry/BVH.cpp
@@ -0,0 +1,27 @@
+#include "k2EngineLowPreCompile.h"
+#include "geometry/BVH.h"
+
+namespace nsK2EngineLow {
+	/// <summary>
+	/// BVHを構築。
+	/// </summary>
+	/// <remark></remark>
+	void BVH::Build()
+	{
+		// ルートノードを作成。
+		m_rootNode = std::make_shared<SNode>();
+		// ノードを内包するAABBを計算する。
+		Vector3 vMax = { -FLT_MAX, -FLT_MAX, -FLT_MAX };
+		Vector3 vMin = {  FLT_MAX,  FLT_MAX,  FLT_MAX };
+		for (auto& node : m_leafNodeArray) {
+			vMax.Max(node->aabb.GetMax());
+			vMin.Min(node->aabb.GetMin());
+		}
+		// ノードのAABBを初期化する。
+		m_rootNode->aabb.Init(vMax, vMin);
+		// 主成分分析を行って、分割平面の法線を決定する。
+		// 分割平面を決定する。
+		Vector3 t = vMax - vMin;
+
+	}
+}
