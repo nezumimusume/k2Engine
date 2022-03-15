0754930796f819168022b9cb5787728abb8df17a
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2022-03-15 18:14:02 +0900

    コメントを整備

--- GameTemplate/k2EngineLow/graphics/Skeleton.cpp
+++ GameTemplate/k2EngineLow/graphics/Skeleton.cpp
@@ -9,15 +9,15 @@ namespace nsK2EngineLow {
 	void Bone::CalcWorldTRS(Vector3& trans, Quaternion& rot, Vector3& scale)
 	{
 		Matrix mWorld = m_worldMatrix;
-		//行列から拡大率を取得する。
+		// 行列から拡大率を取得する。
 		scale.x = mWorld.v[0].Length();
 		scale.y = mWorld.v[1].Length();
 		scale.z = mWorld.v[2].Length();
 		m_scale = scale;
-		//行列から平行移動量を取得する。
+		// 行列から平行移動量を取得する。
 		trans.Set(mWorld.v[3]);
 		m_positoin = trans;
-		//行列から拡大率と平行移動量を除去して回転量を取得する。
+		// 行列から拡大率と平行移動量を除去して回転量を取得する。
 		mWorld.v[0].Normalize();
 		mWorld.v[1].Normalize();
 		mWorld.v[2].Normalize();
@@ -27,7 +27,7 @@ namespace nsK2EngineLow {
 	}
 	Skeleton::Skeleton()
 	{
-		//リザーブ。
+		// リザーブ。
 		m_bones.reserve(BONE_MAX);
 	}
 	Skeleton::~Skeleton()
@@ -46,7 +46,7 @@ namespace nsK2EngineLow {
 	}
 	bool Skeleton::Init(const char* tksFilePath)
 	{
-		//tksファイルをロードする。
+		// tksファイルをロードする。
 		m_tksFile = g_engine->GetTksFileFromBank(tksFilePath);
 		if (m_tksFile == nullptr) {
 			// 新規。
@@ -55,7 +55,7 @@ namespace nsK2EngineLow {
 			g_engine->RegistTksFileToBank(tksFilePath, m_tksFile);
 		}
 		if (m_tksFile->Load(tksFilePath)) {
-			//ボーン行列を構築する。
+			// ボーン行列を構築する。
 			BuildBoneMatrices();
 			return true;
 		}
@@ -64,7 +64,7 @@ namespace nsK2EngineLow {
 	void Skeleton::BuildBoneMatrices()
 	{
 		m_tksFile->QueryBone([&](TksFile::SBone& tksBone) {
-			//バインドポーズ。
+			// バインドポーズ。
 			Matrix bindPoseMatrix;
 			memcpy(bindPoseMatrix.m[0], &tksBone.bindPose[0], sizeof(tksBone.bindPose[0]));
 			memcpy(bindPoseMatrix.m[1], &tksBone.bindPose[1], sizeof(tksBone.bindPose[1]));
@@ -75,7 +75,7 @@ namespace nsK2EngineLow {
 			bindPoseMatrix.m[2][3] = 0.0f;
 			bindPoseMatrix.m[3][3] = 1.0f;
 
-			//バインドポーズの逆行列。
+			// バインドポーズの逆行列。
 			Matrix invBindPoseMatrix;
 			memcpy(invBindPoseMatrix.m[0], &tksBone.invBindPose[0], sizeof(tksBone.invBindPose[0]));
 			memcpy(invBindPoseMatrix.m[1], &tksBone.invBindPose[1], sizeof(tksBone.invBindPose[1]));
@@ -96,8 +96,8 @@ namespace nsK2EngineLow {
 				tksBone.no
 				);
 #if BUILD_LEVEL != BUILD_LEVEL_MASTER
-			//ボーンのバリデーションチェック。
-			//maxScriptでやりたいところではあるが、とりあえずこっち。
+			// ボーンのバリデーションチェック。
+			// maxScriptでやりたいところではあるが、とりあえずこっち。
 			auto it = std::find_if(m_bones.begin(), m_bones.end(), [&](auto& bone) {return wcscmp(boneName, bone->GetName()) == 0;  });
 			if (it != m_bones.end()) {
 				//同名のボーンが見つかった。
@@ -113,7 +113,7 @@ namespace nsK2EngineLow {
 				if (bone->GetParentBoneNo() > m_bones.size())
 					continue;
 				m_bones.at(bone->GetParentBoneNo())->AddChild(bone.get());
-				//ローカルマトリクスを計算。
+				// ローカルマトリクスを計算。
 				const Matrix& parentMatrix = m_bones.at(bone->GetParentBoneNo())->GetInvBindPoseMatrix();
 				Matrix localMatrix;
 				localMatrix = bone->GetBindPoseMatrix() * parentMatrix;
@@ -123,9 +123,7 @@ namespace nsK2EngineLow {
 				bone->SetLocalMatrix(bone->GetBindPoseMatrix());
 			}
 		}
-
-
-		//ボーン行列を確保
+		// ボーン行列を確保
 		m_boneMatrixs = std::make_unique<Matrix[]>(m_bones.size());
 		m_isInited = true;
 
@@ -134,29 +132,29 @@ namespace nsK2EngineLow {
 	void Skeleton::Update(const Matrix& mWorld)
 	{
 		if (m_isPlayAnimation) {
-			//ボーン行列をルートボーンの空間からワールド空間を構築していく。
+			// ボーン行列をルートボーンの空間からワールド空間を構築していく。
 			for (auto& bone : m_bones) {
 				Matrix mBoneWorld;
 				Matrix localMatrix = bone->GetLocalMatrix();
-				//親の行列とローカル行列を乗算して、ワールド行列を計算する。
+				// 親の行列とローカル行列を乗算して、ワールド行列を計算する。
 				mBoneWorld = localMatrix * mWorld;
 				bone->SetWorldMatrix(mBoneWorld);
 			}
 		}
 		else {
-			//アニメーションが流し込まれていると、ボーン行列がルートボーン空間に
-			//変換されているが、流されていないと親の骨の座標系のままなので、
-			//ルートボーン空間→ワールド空間への変換を行う。
+			// アニメーションが流し込まれていると、ボーン行列がルートボーン空間に
+			// 変換されているが、流されていないと親の骨の座標系のままなので、
+			// ルートボーン空間→ワールド空間への変換を行う。
 			for (auto& bone : m_bones) {
 				if (bone->GetParentBoneNo() != -1) {
 					continue;
 				}
-				//ルート。
+				// ルート。
 				UpdateBoneWorldMatrix(*bone, mWorld);
 			}
 		}
 
-		//ボーン行列を計算。
+		// ボーン行列を計算。
 		int boneNo = 0;
 		for (auto& bonePtr : m_bones) {
 			Matrix mBone;
